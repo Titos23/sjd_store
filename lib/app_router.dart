7929b18ebefdcb4253117da9f2f3c9d8038cf668
Titos Stark <titalemson@gmail.com>
2022-05-18 22:51:42 +0100

    Navigation --not finished

---
+++ lib/app_router.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'app_state_manager.dart';
+import 'login_page.dart';
+
+/* This is the main router of the app.
+  It is responsible for the navigation of the application
+*/
+class AppRouter extends RouterDelegate 
+  with ChangeNotifier,PopNavigatorRouterDelegateMixin{
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final AppStateManager appStateManager;
+
+  AppRouter({
+    required this.appStateManager,
+    
+  })
+      : navigatorKey = GlobalKey<NavigatorState>() {
+    // Add Listeners to the router
+      appStateManager.addListener(notifyListeners);
+    
+  }
+    
+  @override
+  void dispose() {
+
+    super.dispose();
+    appStateManager.removeListener(() {
+      notifyListeners();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        if (!appStateManager.isLogin) LoginPage.page()
+      ],
+    );
+    
+  }
+  
+  
+}
