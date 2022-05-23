9e065fd1414b2aecc6d63d32ae284d6f2249cd16
Titos Stark <titalemson@gmail.com>
2022-05-23 22:46:45 +0100

    Login accessible from [AppStateManager]

--- lib/app_state_manager.dart
+++ lib/app_state_manager.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AppStateManager extends ChangeNotifier{
 
@@ -6,9 +8,21 @@ class AppStateManager extends ChangeNotifier{
 
   bool get isLogin => _isLogin;
   
-  void login(String username, String password) {
-    _isLogin = true;
-    notifyListeners();
+  login({required String mail,required pass}) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: mail,
+        password: pass,
+      );
+      _isLogin = true;
+
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:store/app_state_manager.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -17,61 +21,85 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  var _emailController, _passController;
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  
+  
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose;
+    _passController.dispose;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: SingleChildScrollView(
-            reverse: true,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children:   [
-                const SizedBox(height: 10),
-                const FlutterLogo(
-                  size: 100,
-                ),
-                const SizedBox(height: 50),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    labelText:"Email"
+    return Provider(
+      create: (context) => AppStateManager(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: SingleChildScrollView(
+              reverse: true,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children:   [
+                  const SizedBox(height: 10),
+                  const FlutterLogo(
+                    size: 100,
                   ),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                  const SizedBox(height: 50),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      labelText:"Email"
                     ),
-                    labelText: "Password"
                   ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 16),
-                ButtonBar(
-                  children: [
-                    TextButton(
-                      child: const Text("Cancel"),
-                      onPressed: () {},
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: _passController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      labelText: "Password"
                     ),
-                    ElevatedButton(
-                      child: const Text("Submit"),
-                      onPressed: () {},
-                    ),
-                    
-                  ],
-                ),
-              ],
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 16),
+                  ButtonBar(
+                    children: [
+                      TextButton(
+                        child: const Text("Cancel"),
+                        onPressed: () {},
+                      ),
+                      ElevatedButton(
+                        child: const Text("Submit"),
+                        onPressed: Provider.of<AppStateManager>(context, listen: false)
+                        .login(mail: _emailController.Text, pass: _passController.Text),
+                      ),
+                      
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+  
 }
